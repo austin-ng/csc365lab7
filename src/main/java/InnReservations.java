9e7316a3d4ae77c95616db566844d3cf504d06df
Shubham Sengar <shubhamsengar11@Shubhams-MacBook-Pro.local>
2020-06-12 16:53:14 -0700

    fr5 finished

--- src/main/java/InnReservations.java
+++ src/main/java/InnReservations.java
@@ -58,6 +58,7 @@ public class InnReservations {
 	    		case 4:
 	    			break;
 	    		case 5:
+	    			ir.getRevenueSummary();
 	    			break;
 	    		case 6:
 	    			System.out.println("Quit successful");
@@ -319,56 +320,287 @@ public class InnReservations {
 		}
 	}
 
+	private int getRevenueSummary() throws SQLException {
+		try (Connection conn = DriverManager.getConnection(JDBC_URL,
+				JDBC_USER,
+				JDBC_PASSWORD)) {
+			// following query is higher performance but less human-readble so commented out
+			// it also doesnt work if table doesnt have reservations for every month in the year
+//			String sql = "WITH Jan AS (\n" +
+//					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS JanRev\n" +
+//					"    FROM lab7_reservations\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=1\n" +
+//					"    GROUP BY Room\n" +
+//					"),\n" +
+//					"Feb AS (\n" +
+//					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS FebRev\n" +
+//					"    FROM lab7_reservations\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=2\n" +
+//					"    GROUP BY Room\n" +
+//					"),\n" +
+//					"Mar AS (\n" +
+//					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS MarRev\n" +
+//					"    FROM lab7_reservations\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=3\n" +
+//					"    GROUP BY Room\n" +
+//					"),\n" +
+//					"Apr AS (\n" +
+//					"    SELECT DISTINCT R.Room, JanRev, FebRev, MarRev,\n" +
+//					"        ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS AprRev\n" +
+//					"    FROM lab7_reservations AS R LEFT OUTER JOIN Jan ON Jan.Room=R.Room\n" +
+//					"        LEFT OUTER JOIN Feb ON Feb.Room=R.Room\n" +
+//					"        LEFT OUTER JOIN Mar ON Mar.Room=R.Room\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=4\n" +
+//					"    GROUP BY R.Room\n" +
+//					"),\n" +
+//					"May AS (\n" +
+//					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS MayRev\n" +
+//					"    FROM lab7_reservations\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=5\n" +
+//					"    GROUP BY Room\n" +
+//					"),\n" +
+//					"Jun AS (\n" +
+//					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS JunRev\n" +
+//					"    FROM lab7_reservations\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=6\n" +
+//					"    GROUP BY Room\n" +
+//					"),\n" +
+//					"Jul AS (\n" +
+//					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS JulRev\n" +
+//					"    FROM lab7_reservations\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=7\n" +
+//					"    GROUP BY Room\n" +
+//					"),\n" +
+//					"Aug AS (\n" +
+//					"    SELECT DISTINCT R.Room, JanRev, FebRev, MarRev, AprRev, MayRev, JunRev, JulRev,\n" +
+//					"        ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS AugRev\n" +
+//					"    FROM lab7_reservations AS R LEFT OUTER JOIN Apr ON Apr.Room=R.Room\n" +
+//					"        LEFT OUTER JOIN May ON May.Room=R.Room\n" +
+//					"        LEFT OUTER JOIN Jun ON Jun.Room=R.Room\n" +
+//					"        LEFT OUTER JOIN Jul ON Jul.Room=R.Room\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=8\n" +
+//					"    GROUP BY R.Room\n" +
+//					"),\n" +
+//					"Sep AS (\n" +
+//					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS SepRev\n" +
+//					"    FROM lab7_reservations\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=9\n" +
+//					"    GROUP BY Room\n" +
+//					"),\n" +
+//					"Oct AS (\n" +
+//					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS OctRev\n" +
+//					"    FROM lab7_reservations\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=10\n" +
+//					"    GROUP BY Room\n" +
+//					"),\n" +
+//					"Nov AS (\n" +
+//					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS NovRev\n" +
+//					"    FROM lab7_reservations\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=11\n" +
+//					"    GROUP BY Room\n" +
+//					"),\n" +
+//					"Dece AS (\n" +
+//					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS DecRev\n" +
+//					"    FROM lab7_reservations\n" +
+//					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=12\n" +
+//					"    GROUP BY Room\n" +
+//					")\n" +
+//					"SELECT Aug.Room, JanRev, FebRev, MarRev, AprRev, MayRev, JunRev,\n" +
+//					"    JulRev, AugRev, SepRev, OctRev, NovRev, DecRev,\n" +
+//					"    JanRev+FebRev+MarRev+AprRev+MayRev+JunRev+JulRev+AugRev+SepRev+OctRev+NovRev+DecRev AS TotalRevenue\n" +
+//					"FROM Aug LEFT OUTER JOIN Sep ON Sep.Room=Aug.Room\n" +
+//					"    LEFT OUTER JOIN Oct ON Oct.Room=Aug.Room\n" +
+//					"    LEFT OUTER JOIN Nov ON Nov.Room=Aug.Room\n" +
+//					"    LEFT OUTER JOIN Dece ON Dece.Room=Aug.Room;";
+			String sql = "WITH Jan AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS JanRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=1\n" +
+					"    GROUP BY Room\n" +
+					"),\n" +
+					"Feb AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS FebRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=2\n" +
+					"    GROUP BY Room\n" +
+					"),\n" +
+					"Mar AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS MarRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=3\n" +
+					"    GROUP BY Room\n" +
+					"),\n" +
+					"Apr AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS AprRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=4\n" +
+					"    GROUP BY Room\n" +
+					"),\n" +
+					"May AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS MayRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=5\n" +
+					"    GROUP BY Room\n" +
+					"),\n" +
+					"Jun AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS JunRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=6\n" +
+					"    GROUP BY Room\n" +
+					"),\n" +
+					"Jul AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS JulRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=7\n" +
+					"    GROUP BY Room\n" +
+					"),\n" +
+					"Aug AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS AugRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=8\n" +
+					"    GROUP BY Room\n" +
+					"),\n" +
+					"Sep AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS SepRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=9\n" +
+					"    GROUP BY Room\n" +
+					"),\n" +
+					"Oct AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS OctRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=10\n" +
+					"    GROUP BY Room\n" +
+					"),\n" +
+					"Nov AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS NovRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=11\n" +
+					"    GROUP BY Room\n" +
+					"),\n" +
+					"Dece AS (\n" +
+					"    SELECT Room, ROUND(SUM(DATEDIFF(DAY, CheckIn, CheckOut)*Rate)) AS DecRev\n" +
+					"    FROM lab7_reservations\n" +
+					"    WHERE YEAR(CheckOut)=YEAR(CURDATE()) AND MONTH(CheckOut)=12\n" +
+					"    GROUP BY Room\n" +
+					")\n" +
+					"SELECT DISTINCT R.Room, JanRev, FebRev, MarRev, AprRev, MayRev, JunRev,\n" +
+					"    JulRev, AugRev, SepRev, OctRev, NovRev, DecRev,\n" +
+					"    JanRev+FebRev+MarRev+AprRev+MayRev+JunRev+\n" +
+					"        JulRev+AugRev+SepRev+OctRev+NovRev+DecRev AS TotalRevenue\n" +
+					"FROM lab7_reservations AS R LEFT JOIN Jan ON Jan.Room=R.Room\n" +
+					"    LEFT JOIN Feb ON Feb.Room=R.Room\n" +
+					"    LEFT JOIN Mar ON Mar.Room=R.Room\n" +
+					"    LEFT JOIN Apr ON Apr.Room=R.Room\n" +
+					"    LEFT JOIN May ON May.Room=R.Room\n" +
+					"    LEFT JOIN Jun ON Jun.Room=R.Room\n" +
+					"    LEFT JOIN Jul ON Jul.Room=R.Room\n" +
+					"    LEFT JOIN Aug ON Aug.Room=R.Room\n" +
+					"    LEFT JOIN Sep ON Sep.Room=R.Room\n" +
+					"    LEFT JOIN Oct ON Oct.Room=R.Room\n" +
+					"    LEFT JOIN Nov ON Nov.Room=R.Room\n" +
+					"    LEFT JOIN Dece ON Dece.Room=R.Room;";
+
+			try (Statement stmt = conn.createStatement();
+				 ResultSet rs = stmt.executeQuery(sql)) {
+
+				if (!rs.isBeforeFirst()){
+					System.out.println("There have been no reservations in the inn this year.");
+					return 1;
+				}
+
+				System.out.println("Revenue format: Room, Jan, Feb, March, April, May, June, July, " +
+						"Aug, Sept, Oct, Nov, Dec, Total");
+				while (rs.next()) {
+					String room = rs.getString("Room");
+					int jan = rs.getInt("JanRev");
+					int feb = rs.getInt("FebRev");
+					int mar = rs.getInt("MarRev");
+					int apr = rs.getInt("AprRev");
+					int may = rs.getInt("MayRev");
+					int jun = rs.getInt("JunRev");
+					int jul = rs.getInt("JulRev");
+					int aug = rs.getInt("AugRev");
+					int sep = rs.getInt("SepRev");
+					int oct = rs.getInt("OctRev");
+					int nov = rs.getInt("NovRev");
+					int dec = rs.getInt("DecRev");
+					int total = rs.getInt("TotalRevenue");
+					if (total==0){
+						total = jan + feb + mar + apr + may + jun + jul + aug + sep + oct + nov + dec;
+					}
+					System.out.format("%s, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d %n",
+							room, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec, total);
+				}
+			}
+
+		}
+		return 0;
+	}
+
+
     private void initDb() throws SQLException {
 	try (Connection conn = DriverManager.getConnection(JDBC_URL,
 							   JDBC_USER,
 							   JDBC_PASSWORD)) {
 	    try (Statement stmt = conn.createStatement()) {
-	    		stmt.execute("DROP TABLE IF EXISTS lab7_reservations");
-                stmt.execute("DROP TABLE IF EXISTS lab7_rooms");
-                stmt.execute("CREATE TABLE lab7_rooms (" +
-                        "RoomCode char(5) PRIMARY KEY, " +
-                        "RoomName varchar(30) DEFAULT NULL, " +
-                        "Beds int(11) DEFAULT NULL, " +
-                        "bedType varchar(8) DEFAULT NULL, " +
-                        "maxOcc int(11) DEFAULT NULL, " +
-                        "basePrice float DEFAULT NULL, " +
-                        "decor varchar(20) DEFAULT NULL)");
-                stmt.execute("CREATE TABLE lab7_reservations (" +
-                        "CODE int(11) PRIMARY KEY, " +
-                        "Room char(5) REFERENCES lab7_rooms (RoomCode), " +
-                        "CheckIn DATE, " +
-                        "CheckOut DATE, " +
-                        "Rate float, " +
-                        "LastName varchar(15), " +
-                        "FirstName varchar(15), " +
-                        "Adults int(11) DEFAULT NULL, " +
-                        "Kids int(11) DEFAULT NULL, " +
-						"CHECK (CheckIn <= CheckOut))");
-                stmt.execute("INSERT INTO lab7_rooms" +
-                        "(RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor) " +
-                        "VALUES ('AOB', 'Abscond or bolster', 2, 'Queen', 4, 175, 'traditional')");
-                stmt.execute("INSERT INTO lab7_rooms" +
-                        "(RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor) " +
-                        "VALUES ('CAS', 'Convoke and sanguine', 2, 'King', 4, 175, 'traditional')");
-                stmt.execute("INSERT INTO lab7_rooms" +
-                        "(RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor) " +
-                        "VALUES ('FNA', 'Frugal not apropos', 2, 'King', 4, 250, 'traditional')");
-                stmt.execute("INSERT INTO lab7_reservations" +
-                        "(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
-                        "VALUES (10489, 'AOB', '2010-02-02', '2010-02-05', 218.75, 'CARISTO', 'MARKITA', 2, 1)");
-                stmt.execute("INSERT INTO lab7_reservations" +
-                        "(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
-                        "VALUES (10990, 'CAS', '2010-09-21', '2010-09-27', 175, 'TRACHSEL', 'DAMIEN', 1, 3)");
-                stmt.execute("INSERT INTO lab7_reservations" +
-                        "(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
-                        "VALUES (10984, 'AOB', '2010-12-28', '2011-01-01', 201.25, 'ZULLO', 'WILLY', 2, 1)");
-				stmt.execute("INSERT INTO lab7_reservations" +
+			stmt.execute("DROP TABLE IF EXISTS lab7_reservations");
+			stmt.execute("DROP TABLE IF EXISTS lab7_rooms");
+			stmt.execute("CREATE TABLE lab7_rooms (" +
+					"RoomCode char(5) PRIMARY KEY, " +
+					"RoomName varchar(30) DEFAULT NULL, " +
+					"Beds int(11) DEFAULT NULL, " +
+					"bedType varchar(8) DEFAULT NULL, " +
+					"maxOcc int(11) DEFAULT NULL, " +
+					"basePrice float DEFAULT NULL, " +
+					"decor varchar(20) DEFAULT NULL)");
+			stmt.execute("CREATE TABLE lab7_reservations (" +
+					"CODE int(11) PRIMARY KEY, " +
+					"Room char(5) REFERENCES lab7_rooms (RoomCode), " +
+					"CheckIn DATE, " +
+					"CheckOut DATE, " +
+					"Rate float, " +
+					"LastName varchar(15), " +
+					"FirstName varchar(15), " +
+					"Adults int(11) DEFAULT NULL, " +
+					"Kids int(11) DEFAULT NULL, " +
+					"CHECK (CheckIn <= CheckOut))");
+			stmt.execute("INSERT INTO lab7_rooms" +
+					"(RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor) " +
+					"VALUES ('AOB', 'Abscond or bolster', 2, 'Queen', 4, 175, 'traditional')");
+			stmt.execute("INSERT INTO lab7_rooms" +
+					"(RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor) " +
+					"VALUES ('CAS', 'Convoke and sanguine', 2, 'King', 4, 175, 'traditional')");
+			stmt.execute("INSERT INTO lab7_rooms" +
+					"(RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor) " +
+					"VALUES ('FNA', 'Frugal not apropos', 2, 'King', 4, 250, 'traditional')");
+			stmt.execute("INSERT INTO lab7_reservations" +
+					"(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
+					"VALUES (10489, 'AOB', '2010-02-02', '2010-02-05', 218.75, 'CARISTO', 'MARKITA', 2, 1)");
+			stmt.execute("INSERT INTO lab7_reservations" +
+					"(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
+					"VALUES (10990, 'CAS', '2010-09-21', '2010-09-27', 175, 'TRACHSEL', 'DAMIEN', 1, 3)");
+			stmt.execute("INSERT INTO lab7_reservations" +
+					"(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
+					"VALUES (10984, 'AOB', '2010-12-28', '2011-01-01', 201.25, 'ZULLO', 'WILLY', 2, 1)");
+			stmt.execute("INSERT INTO lab7_reservations" +
 					"(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
 					"VALUES (10991, 'CAS', '2020-09-21', '2020-09-27', 175, 'TRACHSEL', 'DAMIEN', 1, 3)");
-				stmt.execute("INSERT INTO lab7_reservations" +
+			stmt.execute("INSERT INTO lab7_reservations" +
 					"(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
-					"VALUES (10992, 'CAS', '2080-09-21', '2080-09-27', 175, 'BIG', 'DATE', 1, 3)");
+					"VALUES (99980, 'CAS', '2020-01-21', '2020-01-27', 175, 'TRACHSEL', 'DAMIEN', 1, 3)");
+			stmt.execute("INSERT INTO lab7_reservations" +
+					"(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
+					"VALUES (99981, 'CAS', '2020-02-21', '2020-02-27', 175, 'TRACHSEL', 'DAMIEN', 1, 3)");
+			stmt.execute("INSERT INTO lab7_reservations" +
+					"(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
+					"VALUES (99982, 'CAS', '2020-03-21', '2020-03-27', 175, 'TRACHSEL', 'DAMIEN', 1, 3)");
+			stmt.execute("INSERT INTO lab7_reservations" +
+					"(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
+					"VALUES (99970, 'FNA', '2020-11-21', '2020-11-27', 500, 'BORGES', 'JORGE', 0, 1)");
+			stmt.execute("INSERT INTO lab7_reservations" +
+				"(CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) " +
+				"VALUES (10992, 'CAS', '2080-09-21', '2080-09-27', 175, 'BIG', 'DATE', 1, 3)");
 	    }
 	}
     }
